fb7fe642982edadf80c93d352ed7f399ed7e23cf
iwanttoeatsogogiverymuch <dlawoersjla@naver.com>
2023-05-10 10:30:22 +0900

    first commit

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
 
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
+
 // Grab the blacklist from the command-line so that we can update the blacklist without deploying
 // again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
@@ -21,6 +25,10 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
+  httpsOptions: {
+    key: fs.readFileSync(path.join(__dirname,'minica-key.pem')),
+    cert: fs.readFileSync(path.join(__dirname , 'minica.pem'))
+  },
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
   requireHeader: ['origin', 'x-requested-with'],
@@ -44,6 +52,6 @@ cors_proxy.createServer({
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
-}).listen(port, host, function() {
+}).listen(port, host, function () {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
